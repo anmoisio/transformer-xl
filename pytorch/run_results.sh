6a6cfa48871c205818eccc0f88067b6387bf2257
Anssi Moisio <anssi.moisio@aalto.fi>
2020-08-13 10:13:13 +0300

    results in separate dir

--- pytorch/run_results.sh
+++ pytorch/run_results.sh
@@ -28,7 +28,7 @@ results () {
     /scratch/work/moisioa3/conv_lm/scripts/combine.py --id2end --input-trn "${filename}" --output-trn "${filename%.txt}"-${n}best-combined.txt
   done
 
-  /scratch/work/moisioa3/conv_lm/scripts/score.sh ${test_set} ${gen_dir}/hypoth_text-${model}_lms{8,9,10,11,12}-${n}best-combined.txt > results-${model}-${test_set}-${n}best.txt
+  /scratch/work/moisioa3/conv_lm/scripts/score.sh ${test_set} ${gen_dir}/hypoth_text-${model}_lms{8,9,10,11,12}-${n}best-combined.txt > results/results-${model}-${test_set}-${n}best.txt
 }
 
 results devel
